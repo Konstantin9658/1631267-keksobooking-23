16769c317794067aee7bce040cce6939e5adb4be
Sokolovskiy Konstantin <Super.Juke88@yandex.ru>
2021-06-01 01:21:24 +0400

    Fix#2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-// Функция, для генерирования случайного дробного числа в заданном диапозоне
+// Функция, для генерирования случайного дробного числа в заданном диапазоне
 const getRandomNumber = function (min, max, precision) {
-  const num = Math.pow(10, precision);
   // Проверяем данные на валидность
   if (max <= min || max < 0 || min < 0) {
     throw new Error('Ожидается max > min, min, max > 0');
   }
   // генерация случайного числа
-  return ~~((Math.random() * (max - min) + min) * num) / num;
+  return (Math.random() * (max - min + 1) + min).toFixed(precision);
 };
 
 const getRandomInt = function (min, max) {
   // Переиспользуем функцию
-  getRandomNumber(min, max, 1);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return getRandomNumber(min, max, 0);
 };
 
+
+getRandomInt(1, 190);
 getRandomNumber(1.1, 1.2, 2);
-getRandomInt(1, 3);
