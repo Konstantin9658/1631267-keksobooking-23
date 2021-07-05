84b439d514dcbfba96d9e347de9d0385cee65fda
Sokolovskiy Konstantin <Super.Juke88@yandex.ru>
2021-07-05 16:46:15 +0400

    Удалил ненужные функции и добавил функции для успешной загрузки карты

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-import {generateAdvert} from './data.js';
 import './form.js';
-import {setAdFormDisabled} from './form.js';
+import {setAdFormDisabled, setUserFormSubmit} from './form.js';
 import {setMapFormDisabled, getMapLoad, createMarker} from './map.js';
+import './status.js';
+import {getData} from './api.js';
+import {showSuccessMessage} from './status.js';
 
 const setPageDisabled = (disabled) => {
   setAdFormDisabled(disabled);
   setMapFormDisabled(disabled);
 };
 
-const addAdvertToMap = () => {
-  setPageDisabled(false);
-  const similarAdverts = new Array(10).fill(null).map(generateAdvert);
-  similarAdverts.forEach((advert) => createMarker(advert));
-};
+setPageDisabled(true);
 
-addAdvertToMap(getMapLoad);
+getMapLoad(() => {
+  setPageDisabled(false);
+  getData(createMarker);
+});
+
+setUserFormSubmit(showSuccessMessage);
